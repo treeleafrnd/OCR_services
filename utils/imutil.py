e87fd84366f1158299e2f96ac11ff4155cd6c0bf
Pravesh22 <budhathokipravesh5@gmail.com>
2023-01-27 11:24:49 +0545

    utils added

---
+++ utils/imutil.py
@@ -0,0 +1,24 @@
+"""
+-- Created by Pravesh Budhathoki
+-- Treeleaf Technologies Pvt. Ltd.
+-- Created on 2023-01-27
+"""
+import os
+
+import cv2
+
+
+def read_image(path):
+    img = cv2.imread(path, 0)
+    return img
+
+
+def get_image(path):
+    images = []
+    print("Importing Images...")
+    for items in os.listdir(path):
+        newPath = path + '/' + items
+        curImg = cv2.imread(newPath, 0)
+        images.append(curImg)
+    print(f"{len(images)} Images imported.")
+    return images
